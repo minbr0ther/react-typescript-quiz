51952bfd38c619b8512778d5aa8ca16dc7e04ad7
minbr0ther <minbr0ther@naver.com>
2022-02-04 20:47:56 +0900

    feat: 정답과 오답을 섞어서 예시답안 생성

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,18 @@
+import QuestionCard from "./component/QuestionCard";
+import { shuffleArray } from "./utils";
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficult: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+// Question 타입을 상속하는 너낌
+export type QuestionState = Question & { answers: string[] };
+
 export enum Difficulty {
   EASY = "easy",
   MEDIUM = "medium",
@@ -10,7 +25,13 @@ export const fetchQuizQuestions = async (
 ) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
 
-  // 왜 wait을 두번해? => 첫번째는 fetch용, 두번째는 jsoin용
+  // 왜 wait을 두번해? => 첫번째는 fetch용, 두번째는 json용
   const data = await (await fetch(endpoint)).json();
-  console.log(data);
+  return data.results.map((question: Question) => ({
+    ...question,
+    answer: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
 };
