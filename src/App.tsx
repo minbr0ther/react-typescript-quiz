2277bfdc5852ce46cb807091f8f6f034c12e8713
minbr0ther <minbr0ther@naver.com>
2022-02-04 19:48:25 +0900

    feat: QuestionCard 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import QuestionCard from "./component/QuestionCard";
 
-function App() {
-  return <div className="App">hi</div>;
-}
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={question[nubmer].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+};
 
 export default App;
