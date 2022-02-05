c7b429da9b3e7c664e52a51821d1acc49e8f948c
minbr0ther <minbr0ther@naver.com>
2022-02-05 10:49:11 +0900

    feat: 조건별로 다르게 보여주기

--- src/component/QuestionCard.tsx
+++ src/component/QuestionCard.tsx
@@ -24,8 +24,8 @@ const QuestionCard: React.FC<Props> = ({
       </p>
       <p dangerouslySetInnerHTML={{ __html: question }} />
       <div>
-        {answers.map((answer) => (
-          <div>
+        {answers.map((answer, index) => (
+          <div key={index}>
             <button disabled={userAnswer} onClick={callback}>
               <span dangerouslySetInnerHTML={{ __html: answer }} />
             </button>
